711e9f7f7c2c471d40925318fee819086bd459c6
Frank A. Krueger <fak@praeclarum.org>
2020-02-29 14:18:32 -0800

    Initial commit

---
+++ Fom/Program.fs
@@ -0,0 +1,58 @@
+﻿open System
+
+module WishfulThinking =
+
+    type Person =
+        {
+            Name : string
+            Age : int
+        }
+
+    let p1 = { Name = "Frank"; Age = 39 }
+
+    let p2 = { Name = "Frank"; Age = 40 }
+
+    let diff = p2 - p1
+
+    match diff.Name with
+    | None -> () // I didn't change my name
+    |
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
